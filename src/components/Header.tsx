109cdd3e0ab7b291c65faaa669444db63a70042b
Tomasz Bursztyński <49883328+bursztynsky@users.noreply.github.com>
2025-12-05 21:50:59 +0100

    add phone button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Header.module.scss';
+import PhoneButton from './PhoneButton';
 
 const Header: React.FC = () => {
     return (
@@ -9,11 +10,15 @@ const Header: React.FC = () => {
                     TwojeLogo
                 </div>
                 <ul className={styles.navList}>
-                    <li><a href="#hero" className={styles.navLink}>Start</a></li>
-                    <li><a href="#features" className={styles.navLink}>Cechy</a></li>
-                    <li><a href="#about" className={styles.navLink}>O Nas</a></li>
-                    <li><a href="#contact" className={styles.navLink}>Kontakt</a></li>
+                    <li><a href="#hotel" className={styles.navLink}>Hotel dla zwierząt</a></li>
+                    <li><a href="#strzyzenie" className={styles.navLink}>Strzyżenie</a></li>
+                    <li><a href="#psychologia" className={styles.navLink}>Psychologia zwierząt</a></li>
+                    <li><a href="#galeria" className={styles.navLink}>Galeria</a></li>
+                    <li><a href="#o-nas" className={styles.navLink}>O nas</a></li>
+                    <li><a href="#cennik" className={styles.navLink}>Cennik</a></li>
+                    <li><a href="#regulamin" className={styles.navLink}>Regulamin</a></li>
                 </ul>
+                <PhoneButton phoneNumber="+48601155887" displayNumber="+48 601 155 887"/>
             </nav>
         </header>
     );
