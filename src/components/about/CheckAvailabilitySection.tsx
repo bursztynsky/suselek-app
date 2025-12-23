d7f49804069bbc8042867ad10b73cf87a2e37599
Tomasz Bursztyński <49883328+bursztynsky@users.noreply.github.com>
2025-12-23 12:55:24 +0100

    fix check availability

--- src/components/about/CheckAvailabilitySection.tsx
+++ src/components/about/CheckAvailabilitySection.tsx
@@ -6,10 +6,10 @@ const CheckAvailabilitySection: React.FC = () => {
     <section id="sprawdz-dostepnosc" className="bg-secondary py-20 px-4">
       <div className="container mx-auto max-w-[900px]">
         <div className="text-center">
-          <h2 className="text-3xl md:text-5xl font-bold text-primary leading-tight mb-8">
+          <h2 className="text-3xl md:text-5xl font-light italic text-text-primary leading-tight mb-8">
             Zwierzęta są w moim życiu od zawsze. Prywatnie mam 3 szynszyle, 2 psy, 1 chomika.
-            Dlatego <span className="font-light italic">wasze zwierzęta</span> traktuję{' '}
-            <span className="font-light italic">jak własne.</span>
+            Dlatego <span className="font-bold">wasze zwierzęta</span> traktuję{' '}
+            <span className="font-bold">jak własne.</span>
           </h2>
           <div className="flex justify-center">
             <Button href="#kontakt" variant="primary" className="font-semibold text-lg">
